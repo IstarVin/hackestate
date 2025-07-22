d158d4881960a6544c3913164fc9ca362126aa80
IstarVin <ajdeleverio20@gmail.com>
2025-07-22 19:24:15 +0800

    db init

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,9 @@
+import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
+
+if (process.env.NODE_ENV) {
+	config({ path: './.env.' + process.env.NODE_ENV });
+}
 
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
@@ -7,5 +12,6 @@ export default defineConfig({
 	dialect: 'postgresql',
 	dbCredentials: { url: process.env.DATABASE_URL },
 	verbose: true,
+	casing: 'snake_case',
 	strict: true
 });
