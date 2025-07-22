0ec94b3eb7a87e341a5266cd4ab721124fb0b4ff
IstarVin <ajdeleverio20@gmail.com>
2025-07-22 20:38:03 +0800

    db init

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,5 @@
 import * as auth from '$lib/server/auth';
-import { db } from '$lib/server/db';
-import type { Handle, ServerInit } from '@sveltejs/kit';
-import { migrate } from 'drizzle-orm/postgres-js/migrator';
-
-export const init: ServerInit = () => {
-	migrate(db, { migrationsFolder: './drizzle' });
-};
+import type { Handle } from '@sveltejs/kit';
 
 const handleAuth: Handle = async ({ event, resolve }) => {
 	const sessionToken = event.cookies.get(auth.sessionCookieName);
